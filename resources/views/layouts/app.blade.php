f91104abc8588215ad0bcd789433f06d3b6f973f
angelavillaester <angelamaevillaester6@gmail.com>
2025-05-16 23:58:19 +0800

    file upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,22 @@ href="https://cdn.jsdelivr.net/npm/bootstrapicons@1.11.1/font/bootstrap-icons.c
  <div class="container">
  <h3 class=" mt-3">Simple Laravel 11 CRUD Application 
 Tutorial</h3>
+@auth 
+ <button>
+    <a href="{{ route('logout') }}"
+         class=" "
+         onclick="event.preventDefault();
+         document.getElementById('logout-form').submit();">
+         <i ></i> Logout
+         <form id="logout-form" action="{{ route('logout') }}"
+         method="POST" class="d-none">
+         @csrf
+         </form>
+
+         
+    </a>
+ </button>
+@endauth
  @yield('content')
  <div class="row justify-content-center text-center mt-3">
  <div class="col-md-12">
